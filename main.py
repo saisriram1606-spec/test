664d94a5d083714480df28841aa401b626cd5b49
saisriram1606-spec <saisriram.1606@gmail.com>
2026-09-07 11:50:04 +0530

    abc change

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ async def root():
     return {"message": "Hello World 123"}
 
 
-@app.get("/hello/{name}")
+@app.get("/helloabc/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
